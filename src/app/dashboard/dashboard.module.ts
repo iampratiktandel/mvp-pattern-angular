177f7b703353af449a110d040765ba7a7fc608ba
iampratiktandel <iampratiktandel@gmail.com>
2022-03-05 17:35:14 +0530

    feat: add header component and dashboard module

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardService } from './dashboard.service';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DashboardModule { }
